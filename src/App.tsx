0bce037d0a6fbb3a08b827d4ba08651173425a14
Bob <rbsavary88@gmail.com>
2023-06-06 19:58:20 -0400

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
+import { Navbar } from "./compnents/Navbar"
 
 
 function App() {
-  return <Container>
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/store" element={<Store />}></Route>
-      <Route path="/about" element={<About />}></Route>
-    </Routes>
-  </Container>
+  
+  
+  return (
+    <>
+    <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
+  )
 }
 
 export default App
