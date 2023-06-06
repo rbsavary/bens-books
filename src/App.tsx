dde490bf573a1a4b8a895f4bd94b5b7c406c4ee7
Bob <rbsavary88@gmail.com>
2023-06-06 19:34:47 -0400

    Add About.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
 
 
 function App() {
