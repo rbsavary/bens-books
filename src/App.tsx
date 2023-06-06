55e8c0bc6538b298be553eadc0bdab14e0047687
Bob <rbsavary88@gmail.com>
2023-06-06 15:23:44 -0400

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { Container } from "react-bootstrap"
 
 
 function App() {
-  return <Container>Hi</Container>
+  return <Container>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/store" element={<Store />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  </Container>
 }
 
 export default App
