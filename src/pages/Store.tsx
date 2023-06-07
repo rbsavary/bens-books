8ce682e528847a7c2e54276e129255c87a04483f
Bob <rbsavary88@gmail.com>
2023-06-06 21:53:58 -0400

    Fix JSON

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,15 @@
+import storeItems from "../data/items.json"
+import { Col, Row } from "react-bootstrap"
+
 export function Store() {
-    return <h1>Store</h1>
+    return (
+        <>
+            <h1>Store</h1>
+            <Row>
+                {storeItems.map(item => (
+                    <Col>{JSON.stringify(item)}</Col>
+                ))}
+            </Row>
+        </>
+    )
 }
